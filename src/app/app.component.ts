bd61004fd69d8039b951b1aa40342e5c5967d4df
bneeli33 <bindhuneeli33@gmail.com>
2020-05-30 19:13:56 -0400

    Added cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Nice tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: '@nature',
+      content: 'I saw this nice tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: '@mountainLover',
+      content: 'Here is apicture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: '@biking1234',
+      content: 'I did some biking today',
+    },
+  ];
 }
